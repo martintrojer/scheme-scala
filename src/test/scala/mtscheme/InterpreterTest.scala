b8fa4f43cba6dde1f11f4e4afac9d0e9349c8fca
Martin Trojer <martin.trojer@gmail.com>
2013-05-27 20:54:31 +0100

    environment code

--- src/test/scala/mtscheme/InterpreterTest.scala
+++ src/test/scala/mtscheme/InterpreterTest.scala
@@ -2,12 +2,37 @@ package mtscheme
 
 import org.scalatest.FunSuite
 
-class InterpreterTest extends FunSuite{
+import mtscheme.Interpreter._
+
+class InterpreterTest extends FunSuite {
+
+  val emptyEnv = List(EnvMap())
+  val entry = ("foo" -> Value(Num(1)))
+
+  test("setEnv") {
+    val resEnv = List(EnvMap(entry))
+    expectResult(resEnv) { setEnv(entry, emptyEnv) }
+  }
 
   test("setEnv on empty env") {
     intercept[IllegalArgumentException] {
-      Interpreter.setEnv("foo", Value(Num(1)), List())
+      setEnv(entry, List())
     }
+  }
+
+  test("simple lookup") {
+    val env = setEnv(entry, emptyEnv)
+    expectResult(Some(Value(Num(1)))) { lookUp("foo", env) }
+  }
+
+  test("nested lookup") {
+    val env = expandEnv(setEnv(entry, emptyEnv))
+    expectResult(Some(Value(Num(1)))) { lookUp("foo", env) }
+  }
+
+  test("failing lookup") {
+    val env = setEnv(entry, emptyEnv)
+    expectResult(None) { lookUp("bar", env) }
   }
 
 }
